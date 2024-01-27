0fa208aa35f30efdebca3b3a4c7fdc38a2d419f5
testgithubtiwari <rt936649@gmail.com>
2024-01-27 11:05:45 +0530

    product with same id will not add

--- lib/firebaseservice.dart
+++ lib/firebaseservice.dart
@@ -8,16 +8,28 @@ class FirebaseService {
   Future<void> addToCart(int id, String testName, String testCount,
       String reportAvailable, String discountPrice, String actualMoney) async {
     try {
-      await _cartReference.push().set({
-        'id': id,
-        'testName': testName,
-        'testCount': testCount,
-        'reportAvailable': reportAvailable,
-        'discountPrice': discountPrice,
-        'actualMoney': actualMoney,
-      });
-      // ignore: avoid_print
-      print('data added');
+      // Check if the item already exists in the cart
+      DataSnapshot snapshot =
+          (await _cartReference.orderByChild('id').equalTo(id).once()).snapshot;
+
+      if (snapshot.value != null) {
+        // Item with the same ID already exists in the cart
+        // Handle the situation, e.g., show a message to the user
+        // ignore: avoid_print
+        print('Item with ID $id already exists in the cart.');
+      } else {
+        // Item doesn't exist, so add it to the cart
+        await _cartReference.push().set({
+          'id': id,
+          'testName': testName,
+          'testCount': testCount,
+          'reportAvailable': reportAvailable,
+          'discountPrice': discountPrice,
+          'actualMoney': actualMoney,
+        });
+        // ignore: avoid_print
+        print('Data added to cart.');
+      }
     } catch (error) {
       // Handle errors, e.g., show a snackbar or log the error
       // ignore: avoid_print
